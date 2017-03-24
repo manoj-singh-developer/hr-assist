d1790f93c6759c2ba3d659130f85b3c3185bdc16
Sebastian Vîrlan <sebastian.virlan@yahoo.com>
2017-03-24 12:24:12 +0200

    Added global config file for application

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,5 +1,5 @@
 GrapeSwaggerRails.options.url      = '/swagger_doc.json'
-GrapeSwaggerRails.options.app_url  = 'http://localhost:3000/api/v1'
+GrapeSwaggerRails.options.app_url  = APP_CONFIG[:APP_URL]
 #GrapeSwaggerRails.options.api_auth     = 'basic' # Or 'bearer' for OAuth
 GrapeSwaggerRails.options.api_key_name = 'token'
 GrapeSwaggerRails.options.api_key_type = 'header'
