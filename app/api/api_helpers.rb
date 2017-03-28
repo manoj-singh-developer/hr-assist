08864d23070b56322e5bc8c1a50dfb1899cb5201
Catalina Sandu <catalina.sandu@assist.ro>
2017-03-28 11:17:06 +0300

    [Task-16338] Modified custom token for authenticated uses

--- app/api/api_helpers.rb
+++ app/api/api_helpers.rb
@@ -12,9 +12,9 @@ module APIHelpers
     user.ensure_authentication_token
     user.save
     data = {:user_id => user.id, :role_id => user.roles.first.id , :token => user.auth_token}
-    user.auth_token = JWT.encode(data, Rails.application.secrets.secret_key_base)
+    custom_token = JWT.encode(data, Rails.application.secrets.secret_key_base)
 
-    success user: user
+    success user: user, custom_token: custom_token
   end
 
   def get_option key
