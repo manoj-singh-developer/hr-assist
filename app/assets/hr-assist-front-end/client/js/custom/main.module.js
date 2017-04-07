31e9417dd6a7c42613128a831a3bc14fcd59639a
Victor JEMAN <victorjeman@gmail.com>
2017-04-07 13:31:13 +0300

    structure changes

---
+++ app/assets/hr-assist-front-end/client/js/custom/main.module.js
@@ -0,0 +1,24 @@
+/*jshint esversion: 6 */
+
+(function() {
+
+  'use strict';
+
+  angular.module('HRA', [
+    'ngMaterial',
+    'ngMessages',
+    'ngFileUpload',
+    'md.data.table',
+    'ngSelectable',
+    'ngSanitize',
+    'ngCsv',
+    'datePicker',
+    'angular-loading-bar',
+    'routes'
+  ]);
+
+  angular
+    .module('HRA')
+    .constant('apiUrl', 'http://192.168.200.115:3000/api/v1');
+
+}());
