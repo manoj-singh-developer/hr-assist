3e0ab2b8ced6c045d5c5096b9e7c2dca152e556c
Catalina Sandu <catalina.sandu@assist.ro>
2017-04-11 11:35:31 +0300

    [Task-16420] New changes for post  endpoint on holidays

--- app/api/v1/holiday_api.rb
+++ app/api/v1/holiday_api.rb
@@ -92,27 +92,41 @@ module V1
       desc "Create new holiday"
       params do
         requires :user_id, allow_blank: false, type: Integer
-        requires :team_leader, allow_blank: false, type: Integer
-        requires :project_id, allow_blank: false, type: Integer
-        requires :replacer_id, allow_blank: false, type: Integer
         requires :start_date, allow_blank: false, type: Date
         requires :end_date, allow_blank: false, type: Date
+        optional :signing_day, type: Date
+        optional :days, type: Integer
+        requires :project_ids, allow_blank: false, type: Array[Integer], desc: "Test using postman [Swagger UI issue]"
+        requires :replacer_ids, allow_blank: false, type: Array[Integer], desc: "Test using postman [Swagger UI issue]"
       end
       post 'new' do
-        project = Project.find_by_id_and_team_leader_id(params[:project_id], params[:team_leader])
-        if project
-          holiday = authorizeAndCreate(Holiday, postParams)
-          holiday_replacement = HolidayReplacement.create(holiday_id: holiday.id, project_id: params[:project_id], replacer_id: params[:replacer_id])
-          {
-            user_id: holiday.user_id,
-            team_leader: holiday_replacement.project.team_leader.name,
-            start_date: holiday.start_date,
-            end_date: holiday.end_date,
-            employee_replacement: [holiday_replacement.replacer_id, holiday_replacement.replacer.name, holiday_replacement.project.name]
-          }
-        else
-          {message: "Project or team leader not found"}
+        User.find(params[:user_id], params[:replacer_id])
+        params[:project_ids].each do |project_id|
+          project = Project.find(project_id)
         end
+        holiday = authorizeAndCreate(Holiday, postParams)
+        params[:project_ids].zip(params[:replacer_ids]).each do |project_id, replacer_id|
+          holiday_replacement = HolidayReplacement.create(holiday_id: holiday.id, project_id: project_id, replacer_id:replacer_id)
+          holiday.holiday_replacements << holiday_replacement
+        end
+        holiday_replacements = holiday.holiday_replacements
+        response = {
+          holiday_id: holiday.id,
+          user_id: holiday.user_id,
+          days: holiday.days,
+          start_date: holiday.start_date,
+          end_date: holiday.end_date,
+          signing_day: holiday.signing_day,
+          employee_replacements:
+            holiday_replacements.map do |holiday_replacement|
+              {
+                team_leader: holiday_replacement.project.team_leader.name,
+                replacer_id: holiday_replacement.replacer_id,
+                replacer_name: holiday_replacement.replacer.name,
+                project_name: holiday_replacement.project.name
+              }
+            end
+        }
       end
 
       desc "Update holiday"
