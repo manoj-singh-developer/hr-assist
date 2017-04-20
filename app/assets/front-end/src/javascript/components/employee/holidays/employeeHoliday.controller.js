9bf29a3f07a1ee48e8d4087a8a4a6a8a9d3b178d
Andrei Robu <andrei.robu@assist.ro>
2017-04-20 17:50:22 +0300

    Task-16432 holidays card update

--- app/assets/front-end/src/javascript/components/employee/holidays/employeeHoliday.controller.js
+++ app/assets/front-end/src/javascript/components/employee/holidays/employeeHoliday.controller.js
@@ -1,31 +1,47 @@
-(function() {
+((_) => {
 
   'use strict';
 
-  // ------------------------------------------------------------------------
-  // @employeeHolidayController
-  // ------------------------------------------------------------------------
 
   angular
     .module('HRA')
     .controller('employeeHolidayController', employeeHolidayController);
 
   employeeHolidayController
-    .$inject = ['$rootScope', '$scope', '$stateParams', 'User', 'autocompleteService', 'miscellaneousService'];
+    .$inject = ['$rootScope', '$stateParams', 'User', 'autocompleteService'];
 
 
-  function employeeHolidayController($rootScope, $scope, $stateParams, User, autocompleteService, miscellaneousService) {
+  function employeeHolidayController($rootScope, $stateParams, User, autocompleteService) {
 
 
-    var vm = this;
+    let vm = this;
+    let days;
     vm.userHolidays;
+    vm.projects = [];
+    vm.users = [];
+    vm.holidaysTable = {
+      order: 'days',
+      limit: 5,
+      page: 1
+    };
+    vm.displayOrHide = false;
 
-    _getUserHolidays();
+    vm.queryProjectSearch = queryProjectSearch;
+    vm.queryUserSearch = queryUserSearch;
+    vm.saveHoliday = saveHoliday;
+
+    $rootScope.$on("event:userResourcesLoaded", (event, data) => {
+      vm.projects = data.projects;
+      vm.users = data.users;
+      _getUserHolidays();
+
+      autocompleteService.buildList(vm.projects, ['name']);
+      autocompleteService.buildList(vm.users, ['first_name', 'last_name']);
+    });
 
     function _getUserHolidays(){
       User.getHolidays($stateParams.id)
         .then((data) => {
-          console.log(data);
           vm.userHolidays = data;
         })
         .catch((error) => {
@@ -33,6 +49,78 @@
         });
     }
 
+    function queryProjectSearch(query) {
+      return autocompleteService.querySearch(query, vm.projects);
+    }
+
+    function queryUserSearch(query){
+      return autocompleteService.querySearch(query, vm.users);
+    }
+
+    function saveHoliday () {
+      let userName = vm.searchUser;
+      let projName = vm.searchProj;
+
+      console.log(vm.signingDate, 'signing date');
+      console.log(vm.from, 'start date');
+      console.log(vm.to, 'end date');
+
+      calculateHolidays(vm.from, vm.to);
+      console.log(days, 'days');
+
+      for(let i = 0; i < vm.users.length; i++){
+        let usersName = vm.users[i].first_name + ' ' + vm.users[i].last_name;
+
+        if(userName === usersName){
+          console.log(vm.users[i].id, 'userID');
+        }
+      }
+
+      for(let j = 0; j < vm.projects.length; j++){
+        let projectsName = vm.projects[j].name;
+
+        if(projName === projectsName){
+          console.log(vm.projects[j].id);
+        }
+      }
+    }
+
+    function calculateHolidays(dDate1, dDate2) {
+
+      let iWeeks, iDateDiff, iAdjust = 0;
+
+      if (dDate2 < dDate1) return -1;
+
+      let iWeekday1 = dDate1.getDay();
+      let iWeekday2 = dDate2.getDay();
+
+      iWeekday1 = (iWeekday1 == 0) ? 7 : iWeekday1;
+      iWeekday2 = (iWeekday2 == 0) ? 7 : iWeekday2;
+
+      if ((iWeekday1 > 5) && (iWeekday2 > 5)) iAdjust = 1;
+
+      iWeekday1 = (iWeekday1 > 5) ? 5 : iWeekday1;
+      iWeekday2 = (iWeekday2 > 5) ? 5 : iWeekday2;
+
+
+      iWeeks = Math.floor((dDate2.getTime() - dDate1.getTime()) / 604800000);
+
+      if (iWeekday1 <= iWeekday2) {
+        iDateDiff = (iWeeks * 5) + (iWeekday2 - iWeekday1)
+      } else {
+        iDateDiff = ((iWeeks + 1) * 5) - (iWeekday1 - iWeekday2)
+      }
+
+      iDateDiff -= iAdjust;
+
+      return days = (iDateDiff + 1);
+
+    }
+
+    vm.displayForm = () => {
+      vm.displayOrHide = !vm.displayOrHide;
+    }
+
   }
 
-}());
+})(_);
