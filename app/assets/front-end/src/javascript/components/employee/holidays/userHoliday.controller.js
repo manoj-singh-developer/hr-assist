dd82cc37964f98c8880d9968a21f5d64d7b09d6c
Daniel Furtuna <daniel.furtuna@assist.ro>
2017-05-12 15:22:16 +0300

    fix datepicker autoscroll, clear inputs on close/save, add validate messages

--- app/assets/front-end/src/javascript/components/employee/holidays/userHoliday.controller.js
+++ app/assets/front-end/src/javascript/components/employee/holidays/userHoliday.controller.js
@@ -41,16 +41,17 @@
     vm.displayOrHide = false;
     vm.replaceInputs = [1];
     vm.dateList = [];
-    vm.minDate = new Date();
     vm.validateDate = false;
+    vm.from = new Date();
+    vm.to = new Date();
+    vm.signingDate = new Date();
 
-
+    vm.clearFields = clearFields;
     vm.checkDates = checkDates;
     vm.queryProjectSearch = queryProjectSearch;
     vm.queryUserSearch = queryUserSearch;
     vm.saveHoliday = saveHoliday;
     vm.addEmptyReplacement = addEmptyReplacement;
-    vm.selectDate = selectDate;
 
     $rootScope.$on("event:userResourcesLoaded", (event, data) => {
       vm.projects = data.projects;
@@ -207,15 +208,13 @@
       }
     }
 
-    // Hack for date picker
-    function selectDate() {
-      $(".md-virtual-repeat-scroller").scrollTop(0);
+    function clearFields() {
+      vm.searchUser = '';
+      vm.searchProj = '';
+      vm.from = undefined;
+      vm.to = undefined;
+      vm.signingDate = undefined;
     }
-    $(".md-datepicker-triangle-button").on('click', function () {
-      setTimeout(function () {
-		    $(".md-virtual-repeat-scroller").scrollTop(0);
-	    }, 100);
-    })
 
     function addEmptyReplacement() {
       vm.replaceInputs.push({});
