8626373ea42443279af17c8ecf9ca701359176a7
Daniel Furtuna <daniel.furtuna@assist.ro>
2017-05-16 17:13:58 +0300

    implement remove btn dialog, fix cancel btn,fix add technology

--- app/assets/front-end/src/javascript/components/employee/projects/employeeProjects.controller.js
+++ app/assets/front-end/src/javascript/components/employee/projects/employeeProjects.controller.js
@@ -6,10 +6,7 @@
     .module('HRA')
     .controller('userProjectsCtrl', userProjectsCtrl);
 
-  userProjectsCtrl
-    .$inject = ['$rootScope', 'autocompleteService', 'Project', 'User'];
-
-  function userProjectsCtrl($rootScope, autocompleteService, Project, User) {
+  function userProjectsCtrl($rootScope, autocompleteService, $mdDialog, Project, User) {
 
     let vm = this;
     let projectsToAdd = [];
@@ -73,10 +70,13 @@
 
     function addInQueue(technology) {
       if (technology) {
-        let toRemove = _.findWhere(technologiesToRemove, { id: technology.id });
-        technologiesToRemove = _.without(technologiesToRemove, toRemove);
-        technologiesToAdd.push(technology.id);
-        vm.userTechnologies.push(technology);
+        let notToAdd = _.findWhere(vm.userTechnologies, { id: technology.id });
+        if (notToAdd === undefined) {
+          let toRemove = _.findWhere(technologiesToRemove, { id: technology.id });
+          technologiesToRemove = _.without(technologiesToRemove, toRemove);
+          technologiesToAdd.push(technology.id);
+          vm.userTechnologies.push(technology);
+        }
         vm.searchTechnology = "";
       }
     }
@@ -88,9 +88,18 @@
       technologiesToRemove.push(technology);
     }
 
-    function deleteProject(project) {
-      User.removeProjects(vm.user, project).then(() => {
-        _getUserProjects();
+    function deleteProject(project, event) {
+      var confirm = $mdDialog.confirm()
+        .title('Would you like to delete ' + project.project.name + ' project?')
+        .targetEvent(event)
+        .ok('Yes')
+        .cancel('No');
+
+      $mdDialog.show(confirm).then(() => {
+        User.removeProjects(vm.user, project).then(() => {
+          _getUserProjects()
+
+        });
       });
     }
 
