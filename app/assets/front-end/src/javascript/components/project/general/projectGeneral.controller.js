b05bc25f886a76716e00a98f039dce832b7b6257
Daniel Furtuna <daniel.furtuna@assist.ro>
2017-05-26 12:38:02 +0300

    some fix

--- app/assets/front-end/src/javascript/components/project/general/projectGeneral.controller.js
+++ app/assets/front-end/src/javascript/components/project/general/projectGeneral.controller.js
@@ -9,35 +9,31 @@
   function projectGeneralCtrl($rootScope, autocompleteService, dateService, Project) {
 
     let vm = this;
-    let projectCoppy = {};
-
     vm.project = {};
     vm.validateDate = false;
     vm.displayOrHide = false;
     vm.dateService = dateService;
 
-    vm.save = save;
-    vm.cancel = cancel;
-    vm.checkDates = checkDates;
-
     $rootScope.$on("event:projectLoaded", (event, data) => {
       vm.project = data;
-      projectCoppy = angular.copy(data);
     });
 
-    function save() {
+    vm.save = () => {
       vm.project.start_date = vm.project.start_date ? vm.dateService.format(vm.project.start_date) : null;
       vm.project.end_date = vm.dateService.format(vm.project.end_date);
       Project.update(vm.project);
       vm.displayOrHide = false;
     }
 
-    function cancel() {
+    vm.cancel = () => {
       vm.displayOrHide = !vm.displayOrHide;
-      vm.project = angular.copy(projectCoppy);
+      Project.getById(vm.project.id)
+        .then((data) => {
+          vm.project = data;
+        });
     }
 
-    function checkDates() {
+    vm.checkDates = () => {
       let startDate = new Date(vm.project.start_date);
       let endDate = new Date(vm.project.end_date);
       if (startDate != undefined && endDate != undefined && startDate > endDate) {
