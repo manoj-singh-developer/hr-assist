ca99a23b979324753b9b133d92cbecea81316f27
Daniel Furtuna <daniel.furtuna@assist.ro>
2017-06-15 16:59:27 +0300

    Add ongoing status on projects

--- app/assets/front-end/src/javascript/components/project/form/projectForm.controller.js
+++ app/assets/front-end/src/javascript/components/project/form/projectForm.controller.js
@@ -5,20 +5,22 @@
   angular
     .module('HRA')
     .controller('projectFormCtrl', projectFormCtrl);
-  projectFormCtrl
-    .$inject = ['$scope', 'Project', '$mdToast', '$mdDialog', '$rootScope'];
 
-  function projectFormCtrl($scope, Project, $mdToast, $mdDialog, $rootScope, project) {
+  function projectFormCtrl($scope, Project, $mdToast, $mdDialog, $rootScope, dateService) {
     let vm = this;
-    vm.project = project || {};
+    vm.project = {};
     vm.validateDate = false;
 
+    vm.dateService = dateService;
     vm.add = add;
     vm.closeButton = closeButton;
     vm.clearButton = clearButton;
     vm.checkDates = checkDates;
+    vm.checkOnGoing = checkOnGoing;
 
     function add() {
+      vm.project.start_date = vm.project.start_date ? vm.dateService.format(vm.project.start_date) : null;
+      vm.project.end_date = vm.project.end_date ? vm.dateService.format(vm.project.end_date) : null;
       if (vm.project.id) {
         Project.update(vm.project).then((data) => {
           $rootScope.$emit('event:projectUpdate', data);
@@ -38,14 +40,24 @@
 
     function clearButton() {
       vm.project = {};
+      vm.onGoing = undefined;
+    }
+
+    function checkOnGoing() {
+      vm.project.end_date = vm.onGoing ? null : vm.project.end_date;
+      return vm.onGoing;
     }
 
     function checkDates() {
-
-      if (vm.project.start_date != undefined && vm.project.end_date != undefined && vm.project.start_date > vm.project.end_date) {
-        vm.validateDate = true;
-      } else {
+      if (vm.onGoing) {
         vm.validateDate = false;
+      } else {
+
+        if (vm.project.start_date != undefined && vm.project.end_date != undefined && vm.project.start_date > vm.project.end_date) {
+          vm.validateDate = true;
+        } else {
+          vm.validateDate = false;
+        }
       }
     }
 
