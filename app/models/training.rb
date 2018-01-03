930df3261725cc2557fa09d6e69830c15dc4f373
alex <alex.prelipcean@yahoo.com>
2018-01-03 16:17:05 +0200

    [Task-15521 Create training_api

--- app/models/training.rb
+++ app/models/training.rb
@@ -1,3 +1,2 @@
 class Training < ApplicationRecord
-  belongs_to :user
 end
---
+++ spec/factories/trainings.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :training do
+    organizer "MyString"
+    subject "MyString"
+    description "MyString"
+    time "2018-01-03 10:26:33"
+  end
+end
