cab9b53fb689d61558cd93ea6ddb7aa9f7bdf2a4
Afrasinei Andrei <andrei.afrasinei@assist.ro>
2018-07-05 14:55:16 +0300

    Fix login bug

--- app/api/v1/users/user_api.rb
+++ app/api/v1/users/user_api.rb
@@ -129,30 +129,25 @@ module V1
         requires :password, type: String
       end
 
-      post "login" do
-        allowed_domains = Domain.all.pluck(:allowed_domain)
-        result = ldap_login
-        if result
-          create_user(result.first)
-        elsif allowed_domains.include?(params[:email].partition('@').last)
-          user = User.find_by_email(params[:email])
-          if user
-            error!('Incorrect Password', 401) if user[:encrypted_password].present? && decrypt(user[:encrypted_password]) != params[:password]
-            if user[:reg_status] == "confirmed"
-              login_non_ldap_user(user)
-            elsif user[:reg_status] == "pending"
-              error!({ message: "Your account is not confirmed." })
-            end
-          else
-            new_user = User.new(email: params[:email], password: params[:password], reg_status: "pending") if User.where(reg_status: "pending").count < 20
-            new_user[:encrypted_password] = encrypt(params[:password])
-            new_user.save
-            success(user: user)
-          end
-        else
+    post "login" do
+      domain = params[:email].partition('@').last
+      allowed_domains = Domain.all.pluck(:allowed_domain)
+      user = User.find_by_email(params[:email])
+      if domain == "assist.ro" || allowed_domains.include?(params[:email].partition('@').last)
+        case login_method(user)
+        when "Ldap login"
+          create_user(ldap_login.first)
+        when "Devise login"
+            login_devise(user)
+        when "Devise create"
+            create_user_devise(user)
+        when "Login error"
           error!({ message: "Authentication FAILED." })
         end
+      else
+        error!({ message: "This domain is missing." })
       end
+    end
 
       # post "login" do
       #   allowed_domains = Domain.all.pluck(:allowed_domain)
