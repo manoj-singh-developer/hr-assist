aa6f621919ca09411b26b01cc0106605fbd4e93f
Afrasinei Andrei <andrei.afrasinei@assist.ro>
2018-07-05 15:20:21 +0300

    Fixing CORS DOMAINS problem

--- config/application.rb
+++ config/application.rb
@@ -17,13 +17,13 @@ module HR
     config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
     config.autoload_paths += %W(#{config.root}/app/policies #{config.root}/app/roles)
 
-    # config.middleware.insert_before 0, Rack::Cors do
-    #   allow do
-    #     origins APP_CONFIG[:CORS_DOMAINS]
-    #     resource '*',
-    #       headers: :any,
-    #       methods: %i(get post put patch delete options head)
-    #   end
-    # end
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins APP_CONFIG[:CORS_DOMAINS]
+        resource '*',
+          headers: :any,
+          methods: %i(get post put patch delete options head)
+      end
+    end
   end
 end
